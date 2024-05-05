57a0241432dd2d826be8d16db6ebe582e62a9279
SonalVikas <sonalvikas.salunkhe@gmail.com>
2024-05-05 12:52:42 +0530

    Personal Blog Website

---
+++ PersonalBlog_Website/js/script.js
@@ -0,0 +1,31 @@
+//Responsive Menu bar
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+ 
+}
+
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  
+}
+//Slider
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); 
+}
+
